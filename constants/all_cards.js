e9e3f521bf1ec9a1459e052760afd925b240ca14
kouphasi <phasi.ok.phasi@gmail.com>
2024-08-29 01:20:10 +0900

    インスタンスを生成するメソッドのconstantsに

--- constants/all_cards.js
+++ constants/all_cards.js
@@ -1,16 +1,16 @@
 
 
 import NumCard from '../model/num_card.js';
-import specialCards from './special_cards.js';
+import specialCardCreators from './special_card_creators.js';
 import colors from '../constants/colors.js';
 
 const availableNumbers = Array.from({length: 10}, (_, i) => i);
 
-const numCards = colors.map( color => availableNumbers.map( num => new NumCard({name: `${color.name}${num}`, num, color}))).flat();
+const numCardCreators = colors.map( color => availableNumbers.map( num => () => new NumCard({name: `${color.name}${num}`, num, color}))).flat();
 
-const allCards = [
-  ...numCards,
-  ...specialCards,
+const CardCreators = [
+  ...numCardCreators,
+  ...specialCardCreators,
 ];
 
-export default allCards;
+export default CardCreators;
